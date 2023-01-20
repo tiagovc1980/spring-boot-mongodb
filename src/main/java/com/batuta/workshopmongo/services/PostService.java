9741e055f9b11370c984d392f8c0722031ea2ff3
tiagovc1980 <vc.tiago@gmail.com>
2023-01-20 15:45:48 -0300

    Obtendo um post por id

---
+++ src/main/java/com/batuta/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.batuta.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.batuta.workshopmongo.domain.Post;
+import com.batuta.workshopmongo.repository.PostRepository;
+import com.batuta.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById (String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
